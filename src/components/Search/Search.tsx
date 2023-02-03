a7ec558bcf179062ed7478c2f87a5773fe88732b
Victor Franco <francovictor.dev@gmail.comgit>
2023-02-03 16:13:56 -0300

    fix: Change subtitle of Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -18,7 +18,7 @@ const Search = ({loadUser}: SearchProps) => {
   return (
     <div className={classes.search}>
       <h2>Busque por um usuário:</h2>
-      <p>Conheça seus principais repositórios</p>
+      <p>Conheça seus repositórios</p>
       <div className={classes.search_container}>
         <input
         type="text"
